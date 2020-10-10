1172965199eaefafdd9c87846b85cb9b4f3c0697
techcoderx <engtianxi@outlook.com>
2020-10-10 21:32:46 +0800

    list account history

--- client/js/account.js
+++ client/js/account.js
@@ -19,9 +19,12 @@ const TransactionTypes = {
     18: 'ENABLE_NODE'
 }
 
+let url = new URL(window.location.href)
 let account = window.location.pathname.substr(2)
 let accountlastupdate = 0
-let accountdata = {}
+let accountdata = null
+let accountHistoryPage = parseInt(url.searchParams.get('page'))
+let historyLoaded = false
 
 $(() => {
     axios.get('https://avalon.oneloved.tube/account/' + account).then((acc) => {
@@ -48,11 +51,7 @@ $(() => {
         }
 
         updateAccount(acc.data)
-
-        $('#acc-loading').hide()
-        $('.spinner-border').hide()
-        $('#acc-container').show()
-
+        display()
         setInterval(()=>reloadAccount((newacc)=>updateAccount(newacc)),10000)
     }).catch((e) => {
         $('#acc-loading').hide()
@@ -61,6 +60,17 @@ $(() => {
             $('#acc-notfound').show()
         else
             $('#acc-error').show()
+    })
+
+    let accountHistoryUrl = 'https://avalon.oneloved.tube/history/' + account + '/0'
+    if (accountHistoryPage != NaN)
+        accountHistoryUrl += '/' + (accountHistoryPage * 50)
+
+    axios.get(accountHistoryUrl).then((history) => {
+        console.log(history.data)
+        $('#acc-history').html(accountHistoryHtml(history.data))
+        historyLoaded = true
+        display()
     })
 })
 
@@ -92,16 +102,13 @@ function updateAccount(acc) {
     }
 }
 
-/*
-<div class="card">
-    <div class="card-header" id="acc-customkey-card-master">
-        <h5 class="mb-0"><button class="btn btn-link" type="button" data-toggle="collapse" data-target="#acc-customkey-collapse-master" aria-expanded="true" aria-controls="acc-customkey-collapse-master">Master</button></h5>
-    </div>
-    <div id="acc-customkey-collapse-master" class="collapse" aria-labelledby="acc-customkey-card-master" data-parent="#acc-customkey">
-        <div class="card-body" id="acc-customkey-det-master"></div>
-    </div>
-</div>
-*/
+function display() {
+    if (account && historyLoaded) {
+        $('#acc-loading').hide()
+        $('.spinner-border').hide()
+        $('#acc-container').show()
+    }
+}
 
 function customKeyHtml(keys) {
     let result = ''
@@ -134,4 +141,79 @@ function leaderVotesHtml(approves) {
     for (let i = 0; i < approves.length; i++)
         result += '<tr><td><a href="https://d.tube/#!/c/' + approves[i] + '">' + approves[i] + '</a></td></tr>'
     return result
+}
+
+function accountHistoryHtml(history) {
+    let result = ''
+    for (let i = 0; i < history.length; i++)
+        for (let j = 0; j < history[i].txs.length; j++) {
+            result += '<div class="card dblocks-card">' + txToString(history[i].txs[j]) + '</div>'
+        }
+    return result
+}
+
+function txToString(tx) {
+    let result = ''
+    switch (tx.type) {
+        case 0:
+            return tx.sender + ' created new account ' + tx.data.name
+        case 1:
+            return tx.sender + ' approved leader ' + tx.data.target
+        case 2:
+            return tx.sender + ' disapproved leader ' + tx.data.target
+        case 3:
+            result = tx.sender + ' transferred ' + (tx.data.amount / 100) + ' DTC to ' + tx.data.receiver
+            if (tx.data.memo)
+                result += ', memo: ' + HtmlSanitizer.SanitizeHtml(tx.data.memo)
+            return result
+        case 4:
+            result = tx.sender
+            if (tx.data.pa && tx.data.pp)
+                result += ' commented on @' + tx.data.pa + '/' + tx.data.pp
+            else
+                result += ' posted a new video @' + tx.sender + '/' + tx.data.link
+            return result
+        case 5:
+            result = tx.sender
+            if (tx.data.vt > 0)
+                result += ' upvoted '
+            else
+                result += ' downvoted '
+            result += '@' + tx.data.author + '/' + tx.data.link + ' with ' + thousandSeperator(tx.data.vt) + ' VP'
+            if (tx.data.tag)
+                result += ' and tagged it with ' + HtmlSanitizer.SanitizeHtml(tx.data.tag)
+            return result
+        case 6:
+            return tx.sender + ' update profile'
+        case 7:
+            return tx.sender + ' subscribed to ' + tx.data.target
+        case 8:
+            return tx.sender + ' unsubscribed to ' + tx.data.target
+        case 10:
+            return tx.sender + ' created a custom key with id ' + HtmlSanitizer.SanitizeHtml(tx.data.id)
+        case 11:
+            return tx.sender + ' removed a custom key with id ' + HtmlSanitizer.SanitizeHtml(tx.data.id)
+        case 12:
+            return tx.sender + ' changed the master key'
+        case 13:
+            result = tx.sender
+            if (tx.data.pa && tx.data.pp)
+                result += ' commented on @' + tx.data.pa + '/' + tx.data.pp
+            else
+                result += ' posted a new video @' + tx.sender + '/' + tx.data.link
+            result += ' and burnt ' + (tx.data.burn / 100) + ' DTC '
+            return result
+        case 14:
+            return tx.sender + ' transferred ' + thousandSeperator(tx.data.amount) + ' VP to ' + tx.data.receiver
+        case 15:
+            return tx.sender + ' transferred ' + thousandSeperator(tx.data.amount) + ' bytes to ' + tx.data.receiver
+        case 16:
+            return tx.sender + ' set a limit on account voting power to ' + tx.data.amount + ' VP'
+        case 17:
+            return tx.sender + ' claimed curation rewards on @' + tx.data.author + '/' + tx.data.link
+        case 18:
+            return tx.sender + ' updated leader key for block production'
+        default:
+            return 'Unknown transaction type ' + tx.type
+    }
 }
