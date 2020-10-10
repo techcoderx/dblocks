5a927d836652f2be8373d140f2214c7d3e54d330
techcoderx <engtianxi@outlook.com>
2020-10-10 21:38:56 +0800

    wip

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ App.use(CORS())
 
 App.get('/',(rq,rp) => loadWebpageFromDisk('client/index.html',rp))
 App.get('/@:account',(rq,rp) => loadWebpageFromDisk('client/account.html',rp))
+App.get('/leaders',(rq,rp) => loadWebpageFromDisk('client/wip.html',rp))
+App.get('/accountprice',(rq,rp) => loadWebpageFromDisk('client/wip.html',rp))
+App.get('/livesubcount',(rq,rp) => loadWebpageFromDisk('client/wip.html',rp))
+App.get('/wip',(rq,rp) => loadWebpageFromDisk('client/wip.html',rp))
 App.get('/404',(rq,rp) => loadWebpageFromDisk('client/404.html',rp))
 
 App.use((req,res) => { return res.status(404).redirect('/404') })
