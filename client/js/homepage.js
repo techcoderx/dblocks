893ebfde545c7c4a206c8fa0f8188d6d84054784
techcoderx <engtianxi@outlook.com>
2020-10-10 14:38:57 +0800

    fix mobile view

--- client/js/homepage.js
+++ client/js/homepage.js
@@ -3,7 +3,10 @@ let unparsedBlocks = 0
 let fetchingBlock = false
 
 $(() => {
-    // Stream blocks and data
+    // Load supply and reward pool, and update every 10 seconds
+    updateChainInfo()
+
+    // Stream blocks
     setInterval(() => {
         axios.get('https://avalon.oneloved.tube/count').then((bHeight) => {
             if (bHeight.data.count > headBlock)
@@ -11,20 +14,6 @@ $(() => {
                     headBlock = bHeight.data.count
                 else
                     unparsedBlocks = bHeight.data.count - headBlock
-        })
-
-        axios.get('https://avalon.oneloved.tube/supply').then((supplyRes) => {
-            $('#supply-circulating').text(thousandSeperator(supplyRes.data.circulating / 100) + ' DTC')
-            $('#supply-unclaimed').text(thousandSeperator(Math.ceil(supplyRes.data.unclaimed) / 100) + ' DTC')
-            $('#supply-total').text(thousandSeperator(Math.ceil(supplyRes.data.total) / 100) + ' DTC')
-        })
-
-        axios.get('https://avalon.oneloved.tube/rewardPool').then((rpRes) => {
-            $('#rp-theo').text(thousandSeperator(rpRes.data.theo / 100) + ' DTC')
-            $('#rp-dist').text(thousandSeperator(Math.ceil(rpRes.data.dist) / 100) + ' DTC')
-            $('#rp-avail').text(thousandSeperator(Math.ceil(rpRes.data.avail) / 100) + ' DTC')
-            $('#rp-burn').text(thousandSeperator(Math.ceil(rpRes.data.burn) / 100) + ' DTC')
-            $('#rp-votes').text(thousandSeperator(Math.ceil(rpRes.data.votes)) + ' VP')
         })
     },3000)
 
@@ -42,7 +31,25 @@ $(() => {
             })
         }
     },500)
+
+    setInterval(updateChainInfo,10000)
 })
+
+function updateChainInfo() {
+    axios.get('https://avalon.oneloved.tube/supply').then((supplyRes) => {
+        $('#supply-circulating').text(thousandSeperator(supplyRes.data.circulating / 100) + ' DTC')
+        $('#supply-unclaimed').text(thousandSeperator(Math.ceil(supplyRes.data.unclaimed) / 100) + ' DTC')
+        $('#supply-total').text(thousandSeperator(Math.ceil(supplyRes.data.total) / 100) + ' DTC')
+    })
+
+    axios.get('https://avalon.oneloved.tube/rewardPool').then((rpRes) => {
+        $('#rp-theo').text(thousandSeperator(rpRes.data.theo / 100) + ' DTC')
+        $('#rp-dist').text(thousandSeperator(Math.ceil(rpRes.data.dist) / 100) + ' DTC')
+        $('#rp-avail').text(thousandSeperator(Math.ceil(rpRes.data.avail) / 100) + ' DTC')
+        $('#rp-burn').text(thousandSeperator(Math.ceil(rpRes.data.burn) / 100) + ' DTC')
+        $('#rp-votes').text(thousandSeperator(Math.ceil(rpRes.data.votes)) + ' VP')
+    })
+}
 
 function newBlockCardHtml(block) {
     let blockCardHtml = '<div class="card dblocks-card">#'
