e7ed0e76a60fa6c235ab68601b97273cccfede8c
techcoderx <engtianxi@outlook.com>
2020-10-10 13:46:43 +0800

    update supply and reward pool live

--- client/js/homepage.js
+++ client/js/homepage.js
@@ -3,29 +3,30 @@ let unparsedBlocks = 0
 let fetchingBlock = false
 
 $(() => {
-    // Get data from API
-    axios.get('https://avalon.oneloved.tube/supply').then((supplyRes) => {
-        $('#supply-circulating').text(thousandSeperator(supplyRes.data.circulating / 100) + ' DTC')
-        $('#supply-unclaimed').text(thousandSeperator(Math.ceil(supplyRes.data.unclaimed) / 100) + ' DTC')
-        $('#supply-total').text(thousandSeperator(Math.ceil(supplyRes.data.total) / 100) + ' DTC')
-    })
+    // Stream blocks and data
+    setInterval(() => {
+        axios.get('https://avalon.oneloved.tube/count').then((bHeight) => {
+            if (bHeight.data.count > headBlock)
+                if (headBlock == 0) 
+                    headBlock = bHeight.data.count
+                else
+                    unparsedBlocks = bHeight.data.count - headBlock
+        })
 
-    axios.get('https://avalon.oneloved.tube/rewardPool').then((rpRes) => {
-        $('#rp-theo').text(thousandSeperator(rpRes.data.theo / 100) + ' DTC')
-        $('#rp-dist').text(thousandSeperator(Math.ceil(rpRes.data.dist) / 100) + ' DTC')
-        $('#rp-avail').text(thousandSeperator(Math.ceil(rpRes.data.avail) / 100) + ' DTC')
-        $('#rp-burn').text(thousandSeperator(Math.ceil(rpRes.data.burn) / 100) + ' DTC')
-        $('#rp-votes').text(thousandSeperator(Math.ceil(rpRes.data.votes)) + ' VP')
-    })
+        axios.get('https://avalon.oneloved.tube/supply').then((supplyRes) => {
+            $('#supply-circulating').text(thousandSeperator(supplyRes.data.circulating / 100) + ' DTC')
+            $('#supply-unclaimed').text(thousandSeperator(Math.ceil(supplyRes.data.unclaimed) / 100) + ' DTC')
+            $('#supply-total').text(thousandSeperator(Math.ceil(supplyRes.data.total) / 100) + ' DTC')
+        })
 
-    // Stream blocks
-    setInterval(() => axios.get('https://avalon.oneloved.tube/count').then((bHeight) => {
-        if (bHeight.data.count > headBlock)
-            if (headBlock == 0) 
-                headBlock = bHeight.data.count
-            else
-                unparsedBlocks = bHeight.data.count - headBlock
-    }),3000)
+        axios.get('https://avalon.oneloved.tube/rewardPool').then((rpRes) => {
+            $('#rp-theo').text(thousandSeperator(rpRes.data.theo / 100) + ' DTC')
+            $('#rp-dist').text(thousandSeperator(Math.ceil(rpRes.data.dist) / 100) + ' DTC')
+            $('#rp-avail').text(thousandSeperator(Math.ceil(rpRes.data.avail) / 100) + ' DTC')
+            $('#rp-burn').text(thousandSeperator(Math.ceil(rpRes.data.burn) / 100) + ' DTC')
+            $('#rp-votes').text(thousandSeperator(Math.ceil(rpRes.data.votes)) + ' VP')
+        })
+    },3000)
 
     setInterval(() => {
         if (unparsedBlocks > 0 && !fetchingBlock) {
