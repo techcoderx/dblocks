2deef8897edcb71039b6f55aeab46241416f5fa6
techcoderx <engtianxi@outlook.com>
2020-10-13 09:09:03 +0800

    dev server cleanup

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-const fs = require('fs')
 const Config = require('./config.json')
 const Express = require('express')
 const CORS = require('cors')
 const App = Express()
 const http = require('http').Server(App)
-
-//const homepage = fs.readFileSync('client/index.html','utf8')
 
 App.use(Express.static(__dirname + '/client', { dotfiles: 'deny' }))
 App.use(CORS())
@@ -22,22 +19,8 @@ App.get('/404',(rq,rp) => loadWebpageFromDisk('client/404.html',rp))
 
 App.use((req,res) => { return res.status(404).redirect('/404') })
 
-function loadWebpage(html,response) {
-    response.writeHead(200, {'Content-Type': 'text/html'})
-    response.write(html)
-    response.end()
-}
-
-function loadWebpageFromDisk(HTMLFile,response) {
-    fs.readFile(HTMLFile,function(error, data) {
-        if (error) {
-            response.status(404).send()
-        } else {
-            response.writeHead(200, {'Content-Type': 'text/html'})
-            response.write(data)
-            response.end()
-        }
-    })
+function loadWebpageFromDisk(file,response) {
+    response.sendFile(__dirname + '/' + file)
 }
 
 http.listen(Config.HTTP_PORT)
