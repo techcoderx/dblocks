32c94c3430398858045eb92852efb8c5d3442f4e
techcoderx <engtianxi@outlook.com>
2021-08-08 15:03:02 +0800

    hotfix 2 countdown

--- js/config.js
+++ js/config.js
@@ -2,10 +2,10 @@ window.config = {
     api: 'https://avalon.oneloved.tube',
     isTestnet: false,
     nextHf: {
-        version: '1.4',
-        codename: 'Harry Potter',
-        block: 8500050,
+        version: '1.5',
+        codename: 'Harry Potter (Hotfix)',
+        block: 8595000,
         releaseSrc: 'GitHub',
-        releaseUrl: 'https://github.com/dtube/avalon/releases/tag/1.4'
+        releaseUrl: 'https://github.com/dtube/avalon/releases/tag/1.5'
     }
 }
