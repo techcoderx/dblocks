cc79a662f5ffa10f0e10ff0d7c50cc63af3a2d84
techcoderx <engtianxi@outlook.com>
2021-08-11 19:27:39 +0800

    properly comment out nextHf

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 window.config = {
     api: 'https://avalon.oneloved.tube',
     isTestnet: false,
-    nextHf: {
-        // version: '1.5',
-        // codename: 'Harry Potter (Hotfix)',
-        // block: 8595000,
-        // releaseSrc: 'GitHub',
-        // releaseUrl: 'https://github.com/dtube/avalon/releases/tag/1.5'
-    }
+    // nextHf: {
+    //     version: '1.5',
+    //     codename: 'Harry Potter (Hotfix)',
+    //     block: 8595000,
+    //     releaseSrc: 'GitHub',
+    //     releaseUrl: 'https://github.com/dtube/avalon/releases/tag/1.5'
+    // }
 }
